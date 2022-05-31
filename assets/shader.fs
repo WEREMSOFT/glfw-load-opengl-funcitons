dd0eff6e2eb042c42b37f4b9636a30e0dd1654dc
weremsoft <pablo.weremczuk@gmail.com>
2022-05-31 23:23:39 +0200

    test shadertoy arto of code raymarching

--- assets/shader.fs
+++ assets/shader.fs
@@ -1,30 +1,77 @@
 #version 330 core
 out vec4 FragColor;
-
 in vec2 TexCoord;
 uniform vec3 iResolution;
+uniform float iTime;
 
-#define RES 16
+/// paste your code here
+#define MAX_STEPS 100
+#define MAX_DIST 100.
+#define SURFACE_DIST .001
 
-float xor(vec2 p)
-{
-    float r = 0.;
-   	for (float i = 1.; i <= 32.; ++i) {
-        float d = pow(2., i);
-    	vec2  s = floor(mod(p / d, 2.));
-        r += ((s.x == s.y) ? 0. : 1.) * d;
-	}    
-    return r;
+float GetDist(vec3 p){
+    vec4 sphere = vec4(0,1,6,1);
+    float dSphere = length(p-sphere.xyz)-sphere.w;
+    float dPlane = p.y;
+    float d = min(dSphere, dPlane);
+    return d;
 }
 
-
-void mainImage(out vec4 fragColor, vec2 fragCoord )
-{
-    float r = pow(2., float(RES));
-    vec2  p = floor(fragCoord.xy / iResolution.xy * r);
-    fragColor = vec4(vec3(xor(p) / r), 1);
+float RayMarch(vec3 ro, vec3 rd){
+    float dO = 0.;
+    for(int i=0; i<MAX_STEPS; i++) {
+    	vec3 p = ro + dO*rd;
+        float dS = GetDist(p);
+        dO += dS;
+        if(dS<SURFACE_DIST || dO>MAX_DIST) break;
+    }
+    return dO;
 }
 
+vec3 GetNormal(vec3 p){
+    vec2 e = vec2(.01, 0);
+    float d = GetDist(p);
+    vec3 n = vec3(
+    	d-GetDist(p-e.xyy),
+        d-GetDist(p-e.yxy),
+        d-GetDist(p-e.yyx)
+    );
+    
+    return normalize(n);
+}
+
+float GetLight(vec3 p){
+    vec3 lightPos = vec3(0, 5, 6);
+    lightPos.xz += vec2(sin(0), cos(0))*2.;
+    vec3 l = normalize(lightPos-p);
+    vec3 n = GetNormal(p);
+    
+    float dif = clamp(dot(n, l), 0., 1.);
+    float d = RayMarch(p+n*SURFACE_DIST*2., l);
+    if (d<length(lightPos-p)) dif *= .1;
+    return dif;
+}
+
+void mainImage( out vec4 fragColor, in vec2 fragCoord )
+{
+    vec2 uv = (fragCoord-.5*iResolution.xy)/iResolution.y;
+
+    vec3 col = vec3(0);
+    
+    vec3 ro = vec3(0,1,0);
+    vec3 rd = normalize(vec3(uv.x, uv.y, 1));
+    
+    float d = RayMarch(ro, rd);
+    
+    vec3 p = ro + rd*d;
+    
+    float dif = GetLight(p);
+    col = vec3(dif);
+
+    fragColor = vec4(col,1.0);
+}
+
+/// leave this function as is
 void main()
 {
 	mainImage(FragColor, TexCoord * iResolution.xy);
